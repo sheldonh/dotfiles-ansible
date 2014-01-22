dd0d9e24ad88c4558fea2c801cff8840038a6d6c
Sheldon Hearn <sheldonh@starjuice.net>
2014-01-22 17:10:44 +0200

    Apply railscasts color scheme to emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,9 @@
-;(setq inhibit-startup-screen t)
+(setq inhibit-startup-screen t)
+
+(require 'color-theme)
+(color-theme-initialize)
+(load-file "~/.emacs.d/color-theme-railscasts.el")
+(color-theme-railscasts)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
