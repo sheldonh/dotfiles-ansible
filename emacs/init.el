8e92e710ba142d369361f2eab15434ef2c3bc1a1
Sheldon Hearn <sheldonh@starjuice.net>
2014-01-23 10:02:30 +0200

    Embiggen emacs

--- emacs/init.el
+++ emacs/init.el
@@ -4,6 +4,7 @@
 (color-theme-initialize)
 (load-file "~/.emacs.d/color-theme-railscasts.el")
 (color-theme-railscasts)
+(set-face-attribute 'default nil :height 160)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
